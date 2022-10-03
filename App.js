5884b93e2a0375f53926079dcdd3808b8562620b
petartrajkovic <petartrajkovic777@gmail.com>
2022-10-03 11:32:50 -0700

    text modified

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ color: '#888', fontSize: 38 }}> To share a photo from your phone with a friend, just press the button below!</Text>
       <StatusBar style="auto" />
     </View>
   );
