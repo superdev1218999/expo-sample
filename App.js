e6acf896d52e763c276dda1d060f368666ea128d
petartrajkovic <petartrajkovic777@gmail.com>
2022-10-03 11:59:59 -0700

    creating a button

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
-import logo from './assets/adaptive-icon.png';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import logo from "./assets/adaptive-icon.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: logo }} style={styles.logo}/>
+      <Image source={{ uri: logo }} style={styles.logo} />
       <Text style={styles.instructions}>
         To share a photo from your phone with a friend, just press the button below!
       </Text>
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -23,11 +28,21 @@ const styles = StyleSheet.create({
   logo: {
     width: 305,
     height: 159,
-    marginBottom: 10,
+    marginBottom: 20,
   },
   instructions: {
     color: '#888',
     fontSize: 18,
     marginHorizontal: 15,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
   },
 });
